9ff924252627ea7f06cc05c0278c5b353c8c48a9
Libeausta <133878238+Libeausta@users.noreply.github.com>
2023-12-05 10:30:15 +0800

    Update SnowFlakes

--- app/src/main/java/com/example/snowflakes/SnowFlakes.kt
+++ app/src/main/java/com/example/snowflakes/SnowFlakes.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.AsyncTask
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
@@ -15,7 +16,8 @@ lateinit var snow: Array<Snowflake>
 val paint = Paint()
 var h = 1000; var w = 1000
 
-class Snowflakes(ctx: Context) : View(ctx) {
+open class Snowflakes(ctx: Context) : View(ctx) {
+    lateinit var moveTask: MoveTask
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawColor(Color.parseColor("#ccf2ff"))
@@ -33,16 +35,16 @@ class Snowflakes(ctx: Context) : View(ctx) {
         val random = Random(0)
         snow = Array(10) {
             Snowflake(
-                random.nextFloat() * w,
-                random.nextFloat() * h,
-                15 + 10 * random.nextFloat(),
-                30 + 20 * random.nextFloat(),
+                x = random.nextFloat() * w,
+                y = random.nextFloat() * h,
+                velocity = 15 + 10 * random.nextFloat(),
+                radius = 30 + 20 * random.nextFloat(),
                 Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256))
             )
         }
     }
 
-    fun MoveSnowflakes() {
+    fun moveSnowflakes() {
         for (s in snow) {
             s.y += s.velocity
             if (s.y > h) {
@@ -50,11 +52,25 @@ class Snowflakes(ctx: Context) : View(ctx) {
                 s.velocity *= 0.95f
             }
         }
+        invalidate()
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        MoveSnowflakes()
-        invalidate()
-        return true
+        //moveSnowflakes()
+        //invalidate()
+        moveTask = MoveTask(this)
+        moveTask.execute(100)
+        return false
+
+    }
+    class MoveTask(val s: Snowflakes) : AsyncTask<Int,Int,Int>() {
+        override fun doInBackground(vararg params: Int?): Int {
+            val delay = params[0] ?: 200
+            while (true) {
+                Thread.sleep(delay.toLong())
+                s.moveSnowflakes()
+            }
+            return 0
+        }
     }
 }
